fd9ee1fd46d7852d9a4c5bd3232dbfa4ff6895b9
TJVisLab <TJVisLab@192-168-153-179.VPN.SHANGHAI.NYU.EDU>
2017-02-20 14:17:03 +0800

    update

---
+++ Chapter 1/1_2.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+using namespace std;
+int main()
+{
+    printf("%.1f\n", 8.0/5.0);
+    return 0;
+}
---
+++ Chapter 3/3_1.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+
+#define maxn 105
+int a[maxn];
+
+int main()
+{
+    int x, n = 0;
+    while(scanf("%d", &x))
+        a[n ++] = x;
+    for(int i = n - 1; i >= 1; i --)
+        printf("%d ", a[i]);
+    printf("%d\n", a[0]);
+    return 0;
+}
